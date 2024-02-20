50284780b3e9ce0c8f3dd087a563df0b210b4638
Antonia Rowlinson <antonia@antonias-air.home>
2024-02-20 12:16:50 +0100

    trial code

--- ExtractCsv.py
+++ ExtractCsv.py
@@ -24,22 +24,4 @@ global db
 # Connect to the database and run the queries
 session = dbtools.access(engine,host,port,user,password,database)
 lightcurves = dbtools.GetAllLightcurves(session,dataset_id)  # get all the lightcurves
-
-
-
-
-
-
-
-
-plotdata = [[VarParams[i].Runningcatalog.id, VarParams[i].Varmetric.eta_int, VarParams[i].Varmetric.v_int, VarParams[i].Varmetric.lightcurve_max, 
-            VarParams[i].Varmetric.lightcurve_median, (VarParams[i].Varmetric.band.freq_central/1e6), VarParams[i].Runningcatalog.datapoints, VarParams[i].Varmetric.newsource, 
-            VarParams[i].Runningcatalog.wm_ra, VarParams[i].Runningcatalog.wm_decl] for i in range(len(VarParams))]
-plotdata = pd.DataFrame(data=plotdata,columns=['runcat','eta','V','maxFlx','avgFlx','freq','dpts','newSrc','ra','dec'])
-plotdata = plotdata.fillna('N')
-
-plotdata = plotdata.loc[(plotdata['eta'] > 0) & (plotdata['V'] > 0) & (plotdata['dpts']>1) & (plotdata['newSrc']=='N')]
-
-# Save the data for plotting
-plotdata.to_csv(outputname+'.csv', index=False)
-
+print(lightcurves)
