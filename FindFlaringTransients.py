9bd5ae5186fd750487692bbc0e4a8550268772ab
antoniar <antonia.rowlinson@gmail.com>
2024-08-12 14:59:02 +0200

    Working python 3 version

--- FindFlaringTransients.py
+++ FindFlaringTransients.py
@@ -6,13 +6,24 @@ import pylab
 import numpy as np 
 import pandas as pd 
 
+import sqlalchemy
+from sqlalchemy import *
+from sqlalchemy.orm import relationship
 
+from dblogin import *
+import tkp.db
+from tkp.db.model import Runningcatalog
+from tkp.db.model import Extractedsource
+from tkp.db.model import Assocxtrsource
+
+global db 
 
 # Inputs to edit
-dataFile = 'ds330_lightcurves.csv' #Insert here the name of the .csv files that need to be analysed
-window_size = 12
+database = '' 
+dataset_ids = [,]
+window_size = 10
 cutoff_val = 3
-minDatapoints = 12
+minDatapoints = 10
 sigma =3. # the number of sigma the deviation has to be above the flux uncertainties
 
 ##############################
@@ -34,11 +45,42 @@ def checkCand(row):
         val = 0
     return val
 
+def GetAllLightcurves(session,dataset_id):
+    # Returns all the light curves for the unique sources in a given dataset
+    x = session.query(Runningcatalog).filter(Runningcatalog.dataset_id == dataset_id)
+    dx = pd.read_sql_query(x.statement,db.connection)
+    dx = dx.rename(index=str,columns={'id' : 'runcat'})
+    y = session.query(Extractedsource,Assocxtrsource).select_from(join(Extractedsource,Assocxtrsource)).filter(Assocxtrsource.runcat_id.in_(dx.runcat)).all()
+    data = [[y[i].Extractedsource.id, y[i].Assocxtrsource.runcat_id, y[i].Extractedsource.image.taustart_ts, y[i].Extractedsource.f_int, y[i].Extractedsource.f_int_err] for i in range(len(y))]
+    data=pd.DataFrame(data=data, columns =['srcID','runcat','time','flux','fluxErr'])
+    data=data.sort_values(['runcat','time'])
+    return(data)
+
+def access(engine,host,port,user,password,database):
+    """ Access the database using sqlalchemy"""
+    # make db global in order to be used in GetPandaExtracted
+    global db
+    db = tkp.db.Database(engine=engine, host=host, port=port,
+                     user=user, password=password, database=database)
+    db.connect()
+    session = db.Session()
+    print ('connected!')
+    return session
+
 ##############################
 
+# Connect to the database and run the queries
+session = access(engine,host,port,user,password,database)
+for dataset_id in dataset_ids:
+    lightcurves = GetAllLightcurves(session,dataset_id)  # get all the lightcurves
+    lightcurves.to_csv('ds'+str(dataset_id)+'_lightcurves.csv', index=False)
 
+data = pd.read_csv('ds'+str(dataset_ids[0])+'_lightcurves.csv')
+dataset_ids.pop(0)
 
-data = pd.read_csv(dataFile)
+for dataset_id in dataset_ids:
+    data=pd.concat([data,pd.read_csv('ds'+str(dataset_id)+'_lightcurves.csv')], ignore_index=True)
+    
 
 finalData = pd.DataFrame(columns = ["srcID","runcat","time","flux","fluxErr","dpts","MAvg","deviation","candidate"])
 runcats = data.runcat.unique()
